e9777fcf184f3c94ef9e48cc9ce198426c695354
Steven McVey <smcvey1210@gmail.com>
2023-03-05 22:44:49 -0800

    commit tree eab6f033af8e875fbc845da35d77a71ad1cc5bf7

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,5 +1,44 @@
+require 'pry'
+
 class Tree
   def initialize(root = nil)
     @root = root
+  end
+
+  def breadth_first_traversal(node)
+    result = []
+    nodes_to_visit = [node]
+
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      result.push(node[:value])
+      nodes_to_visit = nodes_to_visit + node[:children]
+    end
+    result
+  end
+
+  def depth_first_traversal(node)
+    result = []
+    nodes_to_visit = [node]
+
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      result.push(node[:value])
+      nodes_to_visit = node[:children] + nodes_to_visit
+    end
+    result
+  end
+
+  def get_element_by_id(id)
+    nodes_to_visit = @root[:children]
+
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      if node[:id] == id
+        return node
+      end
+      nodes_to_visit = node[:children] + nodes_to_visit
+    end
+    nil
   end
 end
